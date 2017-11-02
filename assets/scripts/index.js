a0b9cf13b883f4c5d91098c46a773d96c7db52f4
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2017-11-02 16:04:47 -0400

    Checks for wins and logs the winner
    console logs winner for horizontal, vertical, and diagonal wins

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,7 +7,7 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
-const marks = ['', '', '', '', '', '', '', '', '']
+const marks = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 const turns = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // const turn = function (marks, id) {
@@ -26,7 +26,7 @@ const turns = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 //   })
 // }
 
-const changeTurn = function () {
+const changeTurn = function () { // this function returns either x or o by shortening the turns array with shift(). alternating starting with o.
   let whosTurn = ''
   for (let i = 0; i < turns.length; i++) {
     if (turns[i] % 2 === 0) {
@@ -42,8 +42,33 @@ const changeTurn = function () {
 }
 
 $('.box').click(function () {
-  $(this).html('<p>' + changeTurn() + '<p>')
+  console.log($(this).html())
+  if (($(this).html() !== 'x') && ($(this).html() !== 'o')) {
+    $(this).html(changeTurn())
+    marks.splice(this.id, 1, $(this).html()) // adds the last x or o to the marks array
+    console.log(marks)
+  } checkWin()
 })
+
+const checkWin = function () {
+  if ((marks[0] === marks[1] && marks[1] === marks[2]) && (marks[0] !== 0)) { // horizontal wins
+    console.log(marks[0] + ' wins!')
+  } else if ((marks[3] === marks[4] && marks[4] === marks[5]) && (marks[3] !== 0)) {
+    console.log(marks[3] + ' wins!')
+  } else if ((marks[6] === marks[7] && marks[7] === marks[8]) && (marks[6] !== 0)) {
+    console.log(marks[6] + ' wins!')
+  } else if ((marks[0] === marks[3] && marks[3] === marks[6]) && (marks[0] !== 0)) { // vertical wins
+    console.log(marks[0] + ' wins!')
+  } else if ((marks[1] === marks[4] && marks[4] === marks[7]) && (marks[0] !== 0)) {
+    console.log(marks[1] + ' wins!')
+  } else if ((marks[2] === marks[5] && marks[5] === marks[8]) && (marks[2] !== 0)) {
+    console.log(marks[2] + ' wins!')
+  } else if ((marks[0] === marks[4] && marks[4] === marks[8]) && (marks[0] !== 0)) { // diagonal wins
+    console.log(marks[0] + ' wins!')
+  } else if ((marks[2] === marks[4] && marks[4] === marks[6]) && (marks[2] !== 0)) {
+    console.log(marks[2] + ' wins!')
+  }
+}
 
 // markBoard(changeTurn())
 
