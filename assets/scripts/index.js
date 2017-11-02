786c3a111bee6158a4ec6992371a8e72cc4edc8a
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2017-11-02 13:03:31 -0400

    Alternates x and o on click
    X starts

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,10 +8,45 @@ $(() => {
 })
 
 const marks = ['', '', '', '', '', '', '', '', '']
+const turns = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const turn = function (marks, id) {
+// const turn = function (marks, id) {
+//   for (let i = 0; i < turns.length; i++) {
+//     if (turn % 2 === 0) {
+//       marks[id] = 'x'
+//     }
+//   }
+// }
 
+// const markBoard = function (turn) {
+//   $('.box').click(function () {
+//     $(this).html('<p>' + turn + '<p>')
+//     turns.shift()
+//     console.log(turns)
+//   })
+// }
+
+const changeTurn = function () {
+  let whosTurn = ''
+  for (let i = 0; i < turns.length; i++) {
+    if (turns[i] % 2 === 0) {
+      whosTurn = 'o'
+      turns.shift()
+      return whosTurn
+    } else {
+      whosTurn = 'x'
+      turns.shift()
+      return whosTurn
+    }
+  }
 }
+
+$('.box').click(function () {
+  $(this).html('<p>' + changeTurn() + '<p>')
+})
+
+// markBoard(changeTurn())
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
